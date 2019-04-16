2c26b2c9220b19ce80891bff5661d1ad13ecdf07
ROBIUL HASAN NOWSHAD <nowshad21aug@outlook.com>
2019-04-16 08:47:49 +0600

    first commit

---
+++ EventPlanner!/EventPlanner!/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EventPlanner_.Models;
+using EventPlanner_.Controllers;
+using EventPlanner_;
+
+
+namespace EventPlanner_.Controllers
+{
+
+	public class HomeController : Controller
+	{
+		Client client = null;
+
+
+		public HomeController()
+		{
+			client = new Client();
+
+		}
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult About()
+		{
+			ViewBag.Message = "About page.";
+
+			return View();
+		}
+
+
+		public ActionResult Service()
+		{
+			ViewBag.Message = "service page.";
+
+			return View();
+		}
+		public ActionResult ClientRegister()
+		{
+			ViewBag.Message = "Register page.";
+
+			return View();
+		}
+
+		
+
+
+
+		public ActionResult Contact()
+		{
+
+			return View();
+		}
+
+
+
+
+
+
+
+
+
+		
+			
+		}
+
+	}
